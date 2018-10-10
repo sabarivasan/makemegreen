aca0ac2379d89fdb6f5bb498d70a9a73d7a9b0e2
Jacob Daniels <jdaniels@cvent.com>
2018-10-10 16:07:40 -0400

    Add incomplete getpoints intent

--- src/Constants.py
+++ src/Constants.py
@@ -3,10 +3,17 @@ EMPTY_OBJ = {}
 
 # Intents
 FIND_GREEN_OPPORTUNITY = "FindGreenOpportunity"
+GET_POINTS = "GetPoints"
 
 # Slots
 OPPORTUNITY_TYPE = "opportunityType"
+POINTS_TIME_PERIOD = "pointsTimePeriod"
 EMAIL_ADDRESS = "emailAddress"
+
+# Point time periods
+DAILY = "daily"
+WEEKLY = "weekly"
+YEARLY = "yearly"
 
 
 SESSION_ATTRS = "sessionAttributes"
---
+++ src/GetPoints.py
@@ -0,0 +1,30 @@
+import LexUtils
+import Constants as CC
+import User
+
+
+def handle(event, content):
+    intent_name = event['currentIntent']['name']
+    slots = event['currentIntent']['slots']
+    session_attrs = event['sessionAttributes'] if event['sessionAttributes'] is not None else {}
+
+    if not LexUtils.is_slot_present(slots, CC.EMAIL_ADDRESS):
+        message = "Can you provide your email address so we can lookup your green history?"
+        return LexUtils.elicit_slot(session_attrs, intent_name, slots, CC.EMAIL_ADDRESS, message, None)
+    email_address = slots[CC.EMAIL_ADDRESS]
+    session_attrs[CC.EMAIL_ADDRESS] = email_address
+
+    user = User.User(email_address)
+
+    points_period_days = 30
+    if LexUtils.is_slot_present(slots, CC.POINTS_TIME_PERIOD):
+        points_time_period = slots[CC.POINTS_TIME_PERIOD]
+        if points_time_period == CC.WEEKLY:
+            points_time_period = 7
+        elif points_time_period == CC.YEARLY:
+            points_time_period = 365
+
+    # TODO get points from user opportunities + points per opportunity
+
+    message = "This has not been implemented yet."
+    return LexUtils.close(CC.EMPTY_OBJ, True, message, CC.EMPTY_OBJ)
